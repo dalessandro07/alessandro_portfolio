16cce02ff9adc95b984c6055574799b3e6f136ad
Alessandro <drios28@outlook.es>
2022-09-02 17:28:47 -0500

    create projects section, add astro-icon dependency, modify site style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				customBlack: '#111111',
+				customWhite: '#f0f0f0'
+			}
+		}
+	},
+	plugins: []
 }
+
