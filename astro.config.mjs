88cca2e3dec0fd1b65a8294427445c9b349fbda0
Alessandro <drios28@outlook.es>
2022-09-03 18:13:28 -0500

    add projects, dark mode, start responsive design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
-import sitemap from '@astrojs/sitemap'
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), sitemap()],
-	vite: {
-		ssr: {
-			external: ['svgo']
-		}
-	}
-})
-
+  integrations: [tailwind(), sitemap(), preact()],
+  vite: {
+    ssr: {
+      external: ['svgo']
+    }
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,42 @@ module.exports = {
 			colors: {
 				customBlack: '#111111',
 				customWhite: '#f0f0f0'
+			},
+			fontSize: {
+				nav: '15px',
+				'7.5xl': '5.25rem'
+			},
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif']
+			},
+			lineHeight: {
+				min: '1.1'
+			},
+			height: {
+				'screen/10': '10vh',
+				'screen/20': '20vh',
+				'screen/30': '30vh',
+				'screen/40': '40vh',
+				'screen/50': '50vh',
+				'screen/60': '60vh',
+				'screen/70': '70vh',
+				'screen/80': '80vh',
+				'screen/90': '90vh'
+			},
+			width: {
+				'screen/10': '10vw',
+				'screen/20': '20vw',
+				'screen/30': '30vw',
+				'screen/40': '40vw',
+				'screen/50': '50vw',
+				'screen/60': '60vw',
+				'screen/70': '70vw',
+				'screen/80': '80vw',
+				'screen/90': '90vw'
 			}
 		}
 	},
-	plugins: []
+	plugins: [],
+	darkMode: 'class'
 }
 
