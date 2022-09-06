41455c1ef5f9cc906e8d135fe778efdf00819f2e
Alessandro <drios28@outlook.es>
2022-09-06 11:49:23 -0500

    configure subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://alessandrorios.netlify.app/',
+	site: 'http://www.alessandrorios.com/',
 	integrations: [tailwind(), sitemap(), preact(), robotsTxt()],
 	vite: {
 		ssr: {
