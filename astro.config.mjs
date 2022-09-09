21343aee1726c0f2d05ec5267d36433485fe5b44
Alessandro <drios28@outlook.es>
2022-09-09 13:24:38 -0500

    add studies, change mobile menu, split code, other fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
 import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import robotsTxt from 'astro-robots-txt'
 import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
+
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+import fonts from 'astro-fonts-next'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://alessandrorios.com/',
-	integrations: [tailwind(), sitemap(), preact(), robotsTxt()],
+	integrations: [
+		tailwind(),
+		sitemap(),
+		preact(),
+		robotsTxt(),
+		fonts({
+			url: [
+				'https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700;900&display=swap',
+				'https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap'
+			]
+		})
+	],
 	vite: {
 		ssr: {
 			external: ['svgo']
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+export interface Project {
+	name: string
+	description: string
+	url: string
+	src: string[]
+	isVideo?: boolean
+	isSecondary?: boolean
+}
