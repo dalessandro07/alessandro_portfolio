acbc692cb4958d05250a97023511950de406e086
Alessandro Rios <drios28@outlook.es>
2023-02-24 09:06:05 -0500

    improve performance, sel host google fonts, add aria-label, lazy loading images, cache css, others.

--- src/components/header/LogoMobile.tsx
+++ src/components/header/LogoMobile.tsx
@@ -1,5 +1,5 @@
 import type { JSX } from "preact/jsx-runtime";
 
 export default function LogoMobile(): JSX.Element {
-	return <img class="w-14 h-14 object-cover" src="/logo.webp" alt="" />;
+	return <img class="w-14 h-14 object-cover" src="/logo.webp" alt="Logo" />;
 }
