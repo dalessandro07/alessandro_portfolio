03c5382bcf863cf68a913b9dd9014e3440134010
Alessandro Rios <drios28@outlook.es>
2023-02-24 09:31:28 -0500

    fix lato css path, improve lazy loading images, include _redirects in dist folder and video preload none

--- src/components/header/LogoMobile.tsx
+++ src/components/header/LogoMobile.tsx
@@ -1,5 +1,13 @@
 import type { JSX } from "preact/jsx-runtime";
 
 export default function LogoMobile(): JSX.Element {
-	return <img class="w-14 h-14 object-cover" src="/logo.webp" alt="Logo" />;
+	return (
+		<img
+			src="/logo.webp"
+			alt="Logo"
+			width="56"
+			height="56"
+			className="w-14 h-14 object-cover"
+		/>
+	);
 }
