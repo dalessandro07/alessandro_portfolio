f1350a583d8cc98aafdade74a11e6ad2dd02ba6a
Alessandro Rios <drios28@outlook.es>
2024-02-28 18:13:39 -0500

    Refactor class order in Footer component and update link URLs (whatsapp and instagram added)

---
+++ src/utils/consts.ts
@@ -0,0 +1,15 @@
+// INDEX
+export const MAIN_TITLE = "Alessandro Rios - Desarrollador Front-End";
+
+export const MAIN_DESCRIPTION =
+	"Alessandro es desarrollador front end y estudiante de computación e informática, especializado en software y con certificaciones en desarrollo web. Disponible para proyectos freelance.";
+
+// RESOURCES
+export const RESOURCES_TITLE =
+	"Recursos para desarrolladores - Alessandro Rios";
+export const RESOURCES_DESCRIPTION =
+	"Recursos que he utilizado mucho a la hora de desarrollar software. Especialmente desarrollo web.";
+
+// 404
+export const PAGE404TITLE = "Página no encontrada";
+export const PAGE404DESCRIPTION = "404 esta página no existe.";
