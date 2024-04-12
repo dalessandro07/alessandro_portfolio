45bd71116eb411438e2d458b998a2f70fdfae8d6
Alessandro Rios <drios28@outlook.es>
2024-04-12 15:33:50 -0500

    Update Astro to v4, update dependencies, add new project mitarjetaapp, improve design and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
-import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import icon from 'astro-icon';
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://alessandrorios.com/',
-	integrations: [tailwind(), sitemap(), preact(), robotsTxt()],
-	vite: {
-		ssr: {
-			external: ['svgo']
-		}
-	}
-})
+  site: 'https://alessandrorios.com/',
+  integrations: [tailwind(), sitemap(), preact(), robotsTxt(), icon()],
+  vite: {
+    ssr: {
+      external: ['svgo']
+    }
+  }
+});
